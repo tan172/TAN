57882102e265a788b43f0bab47ee5a73032a8ead
tan172 <ssagetyp@outlook.jp>
2025-09-22 18:53:28 +0900

    webhook.js の更新

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,11 +1,11 @@
-// pages/api/webhook.js
 export default function handler(req, res) {
   if (req.method === "POST") {
-    console.log("LINE Webhook event:", req.body);
+    console.log("Webhook受信:", req.body);
 
-    // LINEに必ず200返す
-    res.status(200).end();
+    // LINE側に返すレスポンス（必須）
+    res.status(200).send("OK");
   } else {
+    // POST 以外は405で拒否
     res.setHeader("Allow", ["POST"]);
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
